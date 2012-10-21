987fcac1d3f6c34115d6ad8d43730b33ff4bca9b
sirunderground <pascalf79@gmail.com>
2012-10-21 18:29:12 +0000

    [VUE32_2_0] WIP
    git-svn-id: svn+ssh://svn.gel.usherbrooke.ca/grp/svn/f12eq04/Code/Controle/Code_VUE32@352 5c0d3ecc-3e0b-11e1-b269-da1b321ef365

--- VUE32_2_0/src/VUE32_5.c
+++ VUE32_2_0/src/VUE32_5.c
@@ -15,6 +15,7 @@
 #include "VUE32_Utils.h"
 #include "VUE32_Impl.h"
 #include "Board.h"
+
 #include "def.h"
 
 //Interface between hardware and communication
@@ -22,11 +23,13 @@
 extern unsigned int gResourceMemory[256];
 
 unsigned char user_input_VUE32_5 = 0;
+extern volatile unsigned int flag_1ms_a, flag_8ms;
 
 //interrupt.c
 extern volatile unsigned int flag_1ms_a;
 extern volatile unsigned char flag_adc_filter;
 
+unsigned short previous_brake_state = 0, brake_state = 0;
 //VUE32_adc.h
 extern unsigned short adc_raw[ADC_CH][ADC_FILTER];
 extern unsigned short adc_mean[ADC_CH];
@@ -36,13 +39,11 @@ unsigned char ucBrakeSwithRead = 0, ucPreviousBrakeSwithRead = 0, ucBrakeSwithSt
 //Hardware resources manage localy by this VUE32
 HDW_MAPPING gVUE32_5_Ress[] =
 {
-    {E_ID_BRAKEPEDAL, sizeof(unsigned short), Sensor},
-    {E_ID_ACCELERATOR, sizeof(unsigned short), Sensor},
-    {E_ID_STEERINGANGLESENSOR, sizeof(unsigned int), Sensor},
-    {E_ID_DPR, sizeof(unsigned char), Sensor},
-    {E_ID_STATE_SWICHT_TRUNK, sizeof(unsigned char), Sensor},
-    {E_ID_TRUNK_SIGNAL, sizeof(unsigned char), Actuator},
-    {E_ID_SET_LIGTH_STATE, sizeof(unsigned char), Actuator},        //Roof ligth
+    {E_ID_BRAKEPEDAL, 2, 0x00},
+    {E_ID_ACCELERATOR, 2, 0x00},
+    {E_ID_STEERINGANGLESENSOR, 4, 0x00},
+    {E_ID_TRANSMISSIONCONTROL, 2, 0x00},
+    {E_ID_IGNITIONKEY, 2, 0x00},
 };
 
 /*
@@ -52,8 +53,6 @@ void InitVUE32_5(void)
 {
     //Door sensors:
     TRIS_DOOR_TRUNK = 1;
-
-    //CopyValueOn(VUE32_1, E_ID_ACCELERATOR, 2000);
 }
 
 /*
@@ -61,6 +60,7 @@ void InitVUE32_5(void)
  */
 void ImplVUE32_5(void)
 {
+
     if(flag_1ms_a)
     {
         flag_1ms_a = 0;
@@ -72,22 +72,26 @@ void ImplVUE32_5(void)
     {
         flag_adc_filter = 0;
 	filter_adc();
-        gResourceMemory[E_ID_ACCELERATOR] = 10; //read_accelerator(adc_mean[ADC_FILTERED_AN0], adc_mean[ADC_FILTERED_AN1]);
+        gResourceMemory[E_ID_ACCELERATOR] = read_accelerator(adc_mean[ADC_FILTERED_AN0], adc_mean[ADC_FILTERED_AN1]);
         gResourceMemory[E_ID_BRAKEPEDAL] = read_brake(adc_mean[ADC_FILTERED_AN2]);
     }
 
-    //Deboucing Brake Swith
-    ucBrakeSwithRead = (unsigned char)gResourceMemory[E_ID_BRAKEPEDAL] >> 8;
-    if(ucBrakeSwithRead == ucPreviousBrakeSwithRead && ucBrakeSwithState !=ucBrakeSwithRead)
-    {
-        ucBrakeSwithState = ucBrakeSwithRead;
-        EmitAnEvent(E_ID_SET_BRAKE_LIGTH_STATE, VUE32_2, 1, (unsigned char)(gResourceMemory[E_ID_BRAKEPEDAL] >> 8));
-        EmitAnEvent(E_ID_SET_BRAKE_LIGTH_STATE, VUE32_7, 1, (unsigned char)(gResourceMemory[E_ID_BRAKEPEDAL] >> 8));
-    }
-    ucPreviousBrakeSwithRead = ucBrakeSwithRead;
-
-
     //Todo trunk switch
+
+    if(flag_8ms)
+    {
+        flag_8ms = 0;
+
+        brake_state = read_brake(0);
+
+        if( gResourceMemory[E_ID_BRAKEPEDALSWITCH] !=brake_state && brake_state == previous_brake_state)
+        {
+            gResourceMemory[E_ID_BRAKEPEDALSWITCH] = (unsigned int)brake_state;
+            EmitAnEvent(E_ID_SET_BRAKE_LIGTH_STATE, VUE32_2, 1, gResourceMemory[E_ID_BRAKEPEDALSWITCH]);
+            EmitAnEvent(E_ID_SET_BRAKE_LIGTH_STATE, VUE32_7, 1, gResourceMemory[E_ID_BRAKEPEDALSWITCH]);
+        }
+        previous_brake_state = brake_state;
+    }
 }
 
 /*
@@ -99,20 +103,9 @@ void OnMsgVUE32_5(NETV_MESSAGE *msg)
                 ANSWER1(E_ID_BRAKEPEDAL, unsigned short, 5)
                 ANSWER1(E_ID_ACCELERATOR, unsigned short, 5)
                 ANSWER1(E_ID_STEERINGANGLESENSOR, unsigned int, 5)
-                ANSWER1(E_ID_DPR, unsigned char, 5)
+                ANSWER1(E_ID_TRANSMISSIONCONTROL, unsigned short, 5)
+                ANSWER1(E_ID_IGNITIONKEY, unsigned short, 5)
                 LED2 = ~LED2;
-    END_OF_MSG_TYPE
-
-    ON_MSG_TYPE_RTR(VUE32_TYPE_SETVALUE)
-            ACTION1(E_ID_TRUNK_SIGNAL, unsigned char, gResourceMemory[E_ID_TRUNK_SIGNAL]) END_OF_ACTION
-            ACTION1(E_ID_SET_LIGTH_STATE, unsigned char, gResourceMemory[E_ID_SET_LIGTH_STATE]) END_OF_ACTION
-            LED2 = ~LED2;
-    END_OF_MSG_TYPE
-
-    ON_MSG_TYPE( NETV_TYPE_EVENT )
-        ACTION1(E_ID_TRUNK_SIGNAL, unsigned char, gResourceMemory[E_ID_TRUNK_SIGNAL]) END_OF_ACTION
-        ACTION1(E_ID_SET_LIGTH_STATE, unsigned char, gResourceMemory[E_ID_SET_LIGTH_STATE]) END_OF_ACTION
-        LED2 = ~LED2;
     END_OF_MSG_TYPE
 
 }
